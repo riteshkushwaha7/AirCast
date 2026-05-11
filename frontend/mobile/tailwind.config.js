759335b88090af858548de6b1f872a8ac064d29d
riteshkushwaha7 <riteshkushwaha497@gmail.com>
2026-05-11 23:30:04 +0530

    change

--- frontend/mobile/tailwind.config.js
+++ frontend/mobile/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/preset");
+
 module.exports = {
+  presets: [nativewind],
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
